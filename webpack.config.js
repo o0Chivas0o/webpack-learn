bcee07b796ab2a0af5721ed1fa55f79a24c06e76
Lee <326656669@qq.com>
2021-01-23 16:47:44 +0800

    feat: change webpack.config.js setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 module.exports = {
+  // mode: 'development',
   module: {
     rules: [
       {
+        // 匹配规则
         test: /\.jsx?$/,
-        use: ['babel-loader'],
+        // 可以是字符串 use:'babel-loader'
+        // 可以是一个数组 use:['style-loader','css-loader']
+        // 可以是一个对象 如下
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  'corejs': 3
+                }
+              ]
+            ]
+          }
+        },
         // 排除node_modules 目录
         exclude: /node_modules/
       }
